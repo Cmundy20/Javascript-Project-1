405b7c84ebd2827f50ccbecae6bf614946451741
Your Name <you@example.com>
2023-11-09 18:47:06 -0500

    echo "# homework-class-4" >> README.md git init git add README.md git commit -m "first commit" git branch -M main git remote add origin https://github.com/Cmundy20/homework-class-4.git git push -u origin main

---
+++ Class 4/assignment1.js
@@ -0,0 +1,7 @@
+//function photo() {
+//let name = getphoto.link.value;
+
+function photo() {
+  let img = (document.getElementById("showImage").style.display = "block");
+  document.body.appendChild(img);
+}
